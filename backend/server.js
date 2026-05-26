e99c82c277308de236127b0647a8538b3092a8bc
Rathan-Kumar-Gaddam <rathankumar.gaddam@gmail.com>
2026-05-26 21:40:04 +0530

    changed file name connectonDb.js to connectionDb.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 const dotenv = require("dotenv").config()
-const connectDb = require('./config/connectonDb')
+const connectDb = require('./config/connectionDb')
 const PORT = process.env.PORT || 3001
 
 connectDb()
